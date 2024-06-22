c39c271aae7e0710d57c603488634e9ac0cd2434
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-06-23 01:49:37 +0600

    modified files

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,38 @@ import { useEffect } from 'react';
 
 function App() {
   const [qapi, setQapi] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
-    fetch('https://api.quotable.io/random')
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
+    const fetchData = async () => {
+      try {
+        const res = await fetch('https://api.quotable.io/random');
+        if(!res.ok){
+          throw new Error(`HTTP error: Status ${res.status}`);
+        }
+        let data = await res.json();
         setQapi(data);
-      });
+        setErrorMsg(null);
+      } catch(err) {
+        setErrorMsg(err.message);
+        setQapi(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
   }, []);
   return (
     <div className="container">
-      <p id='quote'>{qapi.content}</p>
-      <p id='author'>- {qapi.author}</p>
+      {loading && (
+        <p>Loading</p>
+      )}
+      {errorMsg && (
+        <p>{errorMsg}</p>
+      )}
+      <p id='quote'>{qapi && qapi.content}</p>
+      <p id='author'>{qapi && qapi.author}</p>
       <a href="">New Quote</a>
     </div>
   )
